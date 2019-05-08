5cb7cc1f357c6148c994b064900bbbc7f862695b
Amber Pittman <amber@amberpittman.com>
2019-05-07 21:48:23 -0400

    Working on expandArticle method

--- Article/Article.js
+++ Article/Article.js
@@ -14,14 +14,17 @@ class Article {
     /* Set a click handler on the expandButton reference, calling 
     the expandArticle method.*/
     this.expandButton.addEventListener('click', () => this.expandArticle());
+    this.articleBtnOpen = this.querySelector('.btn-open');
+    this.articleBtnClose = this.querySelector('.btn-close');
   }
 
   expandArticle() {
     /* Using our reference to the domElement, toggle a class 
     to expand or hide the article. */
-    this.domElement.classList.toggle('article-open');
-    this.expandButton.textContent = "Close";
-
+    this.articleBtnOpen.classList.toggle('hide-btn');
+    //this.expandButton.textContent = "Close";
+    this.articleBtnClose.classList.toggle('hide-btn');
+    this.expandButton.classList.toggle('article');
   }
 }
 
