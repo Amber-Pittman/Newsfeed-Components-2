95d5b72df49b3db80e222e582c7531e0aebf75a3
Amber-Pittman <amber@amberpittman.com>
2019-08-13 19:32:52 -0400

    Removed a Hodor paragraph in HTML

--- Article/Article.js
+++ Article/Article.js
@@ -20,7 +20,7 @@ class Article {
     /* Using our reference to the domElement, toggle a class 
     to expand or hide the article. */
     this.domElement.classList.toggle('article-open');
-    //this.domElement.classList.toggle('close');  / Unnecessary
+    //this.domElement.classList.toggle('close');  // Unnecessary
     }
 }
 
